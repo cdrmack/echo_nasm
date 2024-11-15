75759a8be324333e568aba324e6e9c981fc05117
cdrmack <git.pynvt@passmail.net>
2024-11-15 12:00:00 +0100

    calculate msg length

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-HelloWorld using the [nasm](https://nasm.us). Written for FreeBSD.
+HelloWorld using the [nasm](https://nasm.us). Written for FreeBSD, x86_64 instruction set.
 
 ```shell
 gmake
--- hello.asm
+++ hello.asm
@@ -1,16 +1,30 @@
 section .data
-msg	db	'hello, world!', 10, 0
+msg     db       'hello, world!', 10, 0
 
 section .text
-	global _start
+    global  _start
 
 _start:
-	mov	rax, 4		; write syscall
-	mov	rdi, 1		; file descriptor of standard output
-	mov	rsi, msg	; address of the message to write
-	mov	rdx, 15		; message length
-	syscall
+    mov     rbx, msg            ; move the address of msg into rbx
+    mov     rax, rbx            ; move the address in rbx into rax
 
-	mov	rax, 1		; exit syscall
-	mov	rdi, 0		; exit code 0
-	syscall
+nextchar:
+    cmp     byte [rax], 0       ; compare the byte pointed to by rax at this address against zero
+    jz      finished            ; jump to `finished` if the zero flagged is set
+    inc     rax
+    jmp     nextchar
+
+finished:
+    sub     rax, rbx            ; subtract the address in rbx from the address in rax
+                                ; the result is number of segments between them
+                                ; stored inside rax
+
+    mov     rdx, rax            ; message length
+    mov     rax, 4              ; write syscall
+    mov     rdi, 1              ; file descriptor of standard output
+    mov     rsi, msg            ; address of the message to write
+    syscall
+
+    mov     rax, 1              ; exit syscall
+    mov     rdi, 0              ; exit code 0
+    syscall
