394453e53fb8aa5a0b6349e4c28f91ef26fa0d3e
cdrmack <git.pynvt@passmail.net>
2024-11-18 11:30:25 +0100

    add strlen subroutine

--- hello.asm
+++ hello.asm
@@ -5,8 +5,22 @@ section .text
     global  _start
 
 _start:
-    mov     rbx, msg            ; move the address of msg into rbx
-    mov     rax, rbx            ; move the address in rbx into rax
+    mov     rax, msg            ; move the address of msg into rax
+    call    strlen
+
+    mov     rdx, rax            ; strlen leaves the result inside rax, we need it inside rdx for the syscall
+    mov     rax, 4              ; write syscall
+    mov     rdi, 1              ; file descriptor of standard output
+    mov     rsi, msg            ; address of the message to write
+    syscall
+
+    mov     rax, 1              ; exit syscall
+    mov     rdi, 0              ; exit code 0
+    syscall
+
+strlen:
+    push    rbx                 ; push the value in rbx onto the stack to preserve it while we use rbx inside this function
+    mov     rbx, rax            ; move the address in rax into rbx
 
 nextchar:
     cmp     byte [rax], 0       ; compare the byte pointed to by rax at this address against zero
@@ -18,13 +32,5 @@ finished:
     sub     rax, rbx            ; subtract the address in rbx from the address in rax
                                 ; the result is number of segments between them
                                 ; stored inside rax
-
-    mov     rdx, rax            ; message length
-    mov     rax, 4              ; write syscall
-    mov     rdi, 1              ; file descriptor of standard output
-    mov     rsi, msg            ; address of the message to write
-    syscall
-
-    mov     rax, 1              ; exit syscall
-    mov     rdi, 0              ; exit code 0
-    syscall
+    pop     rbx                 ; pop the value on the stack back into rbx
+    ret                         ; return to where the function was called
