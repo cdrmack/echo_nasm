021c1bb791c9504e472ba0c40d970239061fc454
cdrmack <git.pynvt@passmail.net>
2024-11-30 21:42:11 +0100

    count to 10 with iprint procedure

--- hello.asm
+++ hello.asm
@@ -14,27 +14,17 @@ section .text
 global  _start
 
 _start:
-    mov     rax, 123
-    call    iprintlf
+    mov     rcx, 0              ; set rcx to 0
+
+nextnumber:
+    inc     rcx                 ; increment
+
+    mov     rax, rcx            ; store rcx in rax
+    push    rcx
+    call    iprintlf            ; print digit
+    pop     rcx
+
+    cmp     rcx, 10
+    jne     nextnumber
+
     call    exit
-
-;; _start:
-;;     mov     rcx, 0              ; set rcx to 0
-
-;; nextnumber:
-;;     inc     rcx                 ; increment
-
-;;     mov     rax, rcx            ; store rcx in rax
-;;     add     rax, 48             ; add 48, 0 is 48 in ASCII, 1 is 49 and so on...
-;;     push    rax                 ; push rax to the stack so we can get the address in the next instruction
-;;     mov     rax, rsp
-
-;;     push    rcx                 ; store rcx
-;;     call    iprint              ; print digit
-;;     pop     rcx                 ; restore rcx
-
-;;     pop     rax
-;;     cmp     rcx, 10
-;;     jne     nextnumber
-
-;;     call    exit
