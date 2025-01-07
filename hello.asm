475404b03188cb4d656dba97646d72e68788d162
cdrmack <git.pynvt@passmail.net>
2025-01-07 14:09:46 +0100

    loop over all arguments

--- hello.asm
+++ hello.asm
@@ -26,21 +26,14 @@ _start:
 stuff:
     pop     rcx                 ; store argc (top of the stack) in rcx
     pop     rdx                 ; store program name
-    sub     rcx, 1              ; decrease rcx by 1, ignore program name
+    dec     rcx                 ; decrease rcx by 1 (ignore program name)
+    jz      done                ; exit if there were no args
 
-    mov     rax, rdx
-    call    sprintlf            ; print program name
+poparg:
+    pop     rax                 ; store address of string (program argument)
 
-    mov     rax, rcx
-    call    iprintlf            ; print argc - 1
-
-    ;; push    0
-    ;; push    '---'
-    ;; mov     rax, rsp
-    ;; call    sprintlf
-
-    pop     rax                 ; rax should point to string (program argument)
-    ;; xor     rbx, rbx            ; reset lower and upper bytes
+    ;; atoi start
+    xor     rbx, rbx            ; reset lower and upper bytes
     mov     bl, [rax]           ; store actual data in rbx
     cmp     bl, 48              ; 48 is ascii value for 0
     jl      done                ; jump if less
@@ -50,30 +43,10 @@ stuff:
     sub     bl, 48              ; convert ascii to decimal representation
     mov     rax, rbx
     call    iprintlf
+    ;; atoi end
+
+    dec     rcx
+    jnz     poparg
 
 done:
     call    exit
-
-;; nextarg:
-;;     cmp     rcx, 0
-;;     jz      nomoreargs
-
-;;     pop     rax                 ; move argument from stack to rax
-;;     call    atoi
-
-;;     sub     rcx, 1
-;;     jmp     nextarg
-
-
-;;     ;; mov     rax, msg            ; rax stores pointer to msg
-;;     ;; mov     rax, 8
-
-;;     ;; mov     rdx, 0              ; reminder is stored here
-;;     ;; mov     rsi, 10             ; we want to divide by 10
-;;     ;; idiv    rsi                 ; divide rax by rsi, quotient part is in rax, reminder in rdx
-
-;;     ;; add     rdx, 48             ; convert rdx to it's ASCII representation, start of atoi
-;;     ;; mov     rax, rdx            ; move reminder to rax
-
-;; nomoreargs:
-;;     call    exit
