0be0f8256a9a0c2d4ac030580f8eacb3d8f80ee8
cdrmack <git.pynvt@passmail.net>
2025-01-08 12:09:25 +0100

    add 02_proper_exit

---
+++ 02_proper_exit.asm
@@ -0,0 +1,16 @@
+section .data
+msg	db	'hello, world!', 10, 0
+
+section .text
+    global _start
+
+_start:
+    mov	rax, 4		; write syscall
+    mov	rdi, 1		; file descriptor of standard output
+    mov	rsi, msg	; address of the message to write
+    mov	rdx, 15		; message length
+    syscall
+
+    mov	rax, 1		; exit syscall
+    mov	rdi, 0		; exit code 0
+    syscall
