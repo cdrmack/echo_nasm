0990bed66858d51ab5d93970087e0a1fc5c5be4d
cdrmack <git.pynvt@passmail.net>
2025-01-10 02:53:56 +0100

    add 05_include_files

---
+++ 05_include_files.asm
@@ -0,0 +1,27 @@
+;;; Order of sections expected by the linker in FreeBSD:
+;;;   .text: Executable code (first in memory).
+;;; .rodata: Read-only data (constants).
+;;;   .data: Initialized variables.
+;;;    .bss: Uninitialized variables (zeroed by the OS).
+;;;
+;;; Syscall arguments in FreeBSD x86-64:
+;;; rdi, rsi, rdx, r10, r8, r9
+;;; Syscall number is placed in rax.
+
+%include    'functions.asm'
+
+section .text
+    global  _start
+
+_start:
+    mov     rax, msg1           ; move the address of msg into rax
+    call    sprint
+
+    mov     rax, msg2
+    call    sprint
+
+    call    exit
+
+section .data
+    msg1     db  'hello, world!', 10
+    msg2     db  'goodbye!', 10
