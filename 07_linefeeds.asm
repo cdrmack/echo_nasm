c23a8f6db118fe99a34c8757d84bee0983c4cad3
cdrmack <git.pynvt@passmail.net>
2025-01-13 14:20:20 +0100

    add 07_linefeeds

---
+++ 07_linefeeds.asm
@@ -0,0 +1,27 @@
+;;; Order of sections expected by the linker in FreeBSD:
+;;;   .text: Executable code (first in memory).
+;;; .rodata: Read-only data (constants).
+;;;   .data: Initialized variables.
+;;;    .bss: Uninitialized variables (zeroed by the OS).
+;;;
+;;; Syscall arguments in FreeBSD x86-64:
+;;; rdi, rsi, rdx, r10, r8, r9
+;;; Syscall number is placed in rax.
+
+%include    'functions.asm'
+
+section .text
+    global  _start
+
+_start:
+    mov     rax, msg1
+    call    sprintlf
+
+    mov     rax, msg2
+    call    sprintlf
+
+    call    exit
+
+section .data
+    msg1    db  'hello, world!', 0
+    msg2    db  'bye, world!', 0
